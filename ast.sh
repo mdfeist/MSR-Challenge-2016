f59752769b52763907b2082a99f71f1197f80e3c
Ubuntu <mdfeist@ualberta.ca>
2016-02-10 17:51:35 -0800

    Update ash.sh

--- ast.sh
+++ ast.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-is_java="*.java^"
-valid=false
-
-if [[ $1 =~ $is_java ]]; then
-    valid=true
-fi
-
-if [[ $2 =~ $is_java ]]; then
-    valid=true
-fi
-
-if [ $valid ]
+if [ ${1: -5} == ".java" ] || [ ${2: -5} == ".java" ]
 then
-	exit
+    echo $1
+	echo $2
+
+	sh runDiff.sh $1 $2
 fi
-
-echo $1
-echo $2
-
-java -cp ~/Windows/Documents/UofA/CMPUT664/Assignment2/gumtree-spoon-ast-diff/target/gumtree-spoon-ast-diff-0.0.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.sacha.spoon.diffSpoon.DiffSpoonImpl $1 $2
---
+++ runDiff.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp ~/Windows/Documents/UofA/CMPUT664/Assignment2/gumtree-spoon-ast-diff/target/gumtree-spoon-ast-diff-0.0.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.sacha.spoon.diffSpoon.DiffSpoonImpl $1 $2
