9ea2f827d433d3f8b70b32c914b4334c969e4b2e
Ubuntu <mdfeist@ualberta.ca>
2016-02-10 17:59:46 -0800

    Fixed ast issue.

--- ast.sh
+++ ast.sh
@@ -5,5 +5,5 @@ then
     echo $1
 	echo $2
 
-	sh runDiff.sh $1 $2
+	java -cp ~/Windows/Documents/UofA/CMPUT664/Assignment2/gumtree-spoon-ast-diff/target/gumtree-spoon-ast-diff-0.0.3-SNAPSHOT-jar-with-dependencies.jar fr.inria.sacha.spoon.diffSpoon.DiffSpoonImpl $1 $2
 fi
